40498fb317b6a3566c535bd7fe1dd7084014ed00
Palash Das <palashkok3@gmail.com>
2018-09-22 15:23:57 +0530

    This solution is accepted
    I guess the order of the call is important here, also I've use faster DS for memoization.

--- src/com/company/SubsetSum.java
+++ src/com/company/SubsetSum.java
@@ -1,32 +1,34 @@
-package com.company;
+/*package whatever //do not write package name here */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Objects;
+import java.util.Map;
+import java.util.function.Supplier;
 
-public class SubsetSum {
+class GFG {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Long t = Long.parseLong(br.readLine());
-        HashMap<State, Boolean> map;
+        Map<Integer, Boolean> maps[];
 
         for (int i = 0; i < t; i++) {
             int numberOfElements = Integer.parseInt(br.readLine());
-            map = new HashMap<>();
+            maps = new HashMap[101];
             String[] split = br.readLine().split(" ");
 
             int[] numbers = new int[numberOfElements];
+            int sum = 0;
             for (int i1 = 0; i1 < numberOfElements; i1++) {
                 numbers[i1] = Integer.parseInt(split[i1]);
+                sum += numbers[i1];
             }
 
-            int bucket1 = 0;
-            int bucket2 = 0;
+            int remainingSumForBucket1 = sum / 2;
 
-            boolean x = checkEquality(numbers, 0, bucket1, bucket2, numberOfElements, map);
+            boolean x = (sum % 2 == 0) && checkEquality(numbers, 0, remainingSumForBucket1, numberOfElements, maps);
             if (x) {
                 System.out.println("YES");
             } else {
@@ -35,60 +37,28 @@ public class SubsetSum {
         }
     }
 
-    public static boolean checkEquality(int[] numbers, int index, int bucket1, int bucket2, int numberOfElements,
-        HashMap<State, Boolean> map) {
+    public static boolean checkEquality(int[] numbers, int index, int remainingSumForBucket1, int numberOfElements,
+        Map<Integer, Boolean> maps[]) {
+        if (maps[index] == null)
+            maps[index] = new HashMap<>();
+        Map<Integer, Boolean> map = maps[index];
 
-        State presentState = new State(index, bucket1, bucket2);
-        if (map.containsKey(presentState))
-            return map.get(presentState);
+        if (map.containsKey(remainingSumForBucket1))
+            return map.get(remainingSumForBucket1);
 
-        if (index == numberOfElements) {
-            map.put(presentState, false);
+        if (remainingSumForBucket1 < 0 || index >= numberOfElements) {
+            map.put(remainingSumForBucket1, false);
             return false;
         }
 
-        if ((bucket1 - bucket2 == numbers[index]) || (bucket2 - bucket1 == numbers[index])) {
-            map.put(presentState, true);
-            return true;
-        }
+        Supplier<Boolean> takeCurrentNoToBucket1 = () -> checkEquality(numbers, index + 1, remainingSumForBucket1 - numbers[index], numberOfElements, maps);
+        Supplier<Boolean> takeCurrentNoToBucket2 = () -> checkEquality(numbers, index + 1, remainingSumForBucket1, numberOfElements, maps);
 
-        if (bucket1 != 0 && bucket1 == bucket2) {
-            map.put(presentState, true);
-            return true;
-        }
+        boolean possible = remainingSumForBucket1 == 0
+            || takeCurrentNoToBucket1.get()     // DONT YOU DARE TO FLIP THE ORDER OF THESE CALLS
+            || takeCurrentNoToBucket2.get();    // CALLING THIS BEFORE WILL TAKE 7x MORE TIME
 
-        boolean withThisNumberInBucket1 = checkEquality(numbers, index + 1, bucket1 + numbers[index], bucket2, numberOfElements, map);
-        boolean withThisNumberInBucket2 = checkEquality(numbers, index + 1, bucket1, bucket2 + numbers[index], numberOfElements, map);
-        boolean possible = withThisNumberInBucket1 || withThisNumberInBucket2;
-        map.put(presentState, possible);
+        map.put(remainingSumForBucket1, possible);
         return possible;
-    }
-
-    static class State {
-        int index;
-        int bucket1;
-        int bucket2;
-
-        public State(int index, int bucket1, int bucket2) {
-            this.index = index;
-            this.bucket1 = bucket1;
-            this.bucket2 = bucket2;
-        }
-
-        @Override public boolean equals(Object o) {
-            if (this == o)
-                return true;
-            if (o == null || getClass() != o.getClass())
-                return false;
-            State state = (State) o;
-            return index == state.index &&
-                bucket1 == state.bucket1 &&
-                bucket2 == state.bucket2;
-        }
-
-        @Override public int hashCode() {
-
-            return Objects.hash(index, bucket1, bucket2);
-        }
     }
 }
